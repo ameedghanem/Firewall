82af5ce0d9d045922b676a538ba044c41cb27a27
ameedghanem <57847235+ameedghanem@users.noreply.github.com>
2020-05-25 00:57:05 +0300

    Add files via upload

--- homeworks/geo_qa.py
+++ homeworks/geo_qa.py
@@ -13,29 +13,29 @@ area = infobox.xpath("//area//text()")[0].replace(" ", "_")
 government = infobox.xpath("//government//text()")[0].replace(" ", "_")
 capital = infobox.xpath("//capital//text()")[0].replace(" ", "_")
 """
-countries_map = {}# a dictionary which will map a country to a list of its relevant elemtns (presidnet, capital, etc..)
+countries_map = {}# a dictionary which will map a country to a list of its relevant elemtns (president, capital, etc..)
 
 #########################
 # Information Extraction
 #########################
 
 
-def get_cherecter_info(url):
+def get_charecter_info(url):
 	"""
 	Accepts a url of some president.
 	Returns his name and his date of birth
 	"""
 	res = requests.get(url)
 	doc = lxml.html.fromstring(res.content)
-	infobox = doc.xpath("//table[contains(@class, 'infobox')]")[0].replace(" ", "_")
+	infobox = doc.xpath("//table[contains(@class, 'infobox')]")[0]
 
 	# extract the charecter name
 	#name = infobox.xpath("//table//div[@class='fn')]/text()")[0].replace(" ", "_")
 
 	# extract the date of birth
-	b = infobox.xpath("//table//th[contains(text(), 'Born')]")[0].replace(" ", "_")
+	b = infobox.xpath("//table//th[contains(text(), 'Born')]")
 	if b != []:
-		bdate = xpath("./../td//span[@class='bday']//text()")[0].replace(" ", "_")
+		bdate = b[0].xpath("./../td//span[@class='bday']//text()")[0].replace(" ", "_")
 
 	return bdate
 
@@ -57,6 +57,10 @@ def get_countries_info(url):
 
 	with open("ontology.nt", 'a+') as writer:
 		for c in countries:
+			# some of these fields are empty in some of the countries. Thus, we need to initialize them in each iteration
+			president = prime_minister = population = area = government = capital = ""
+			pt_bdate = pm_bdate = ""
+
 			page = requests.get(wiki_prefix + c)
 			page_doc = lxml.html.fromstring(page.content)
 			infobox = page_doc.xpath("//table[contains(@class, 'infobox')]")[0]
@@ -65,40 +69,50 @@ def get_countries_info(url):
 
 			president_h = infobox.xpath("//th//div/a[contains(@title, 'President of')]")
 			if president_h != []:
-				print(president_h)
-				president = president_h[0].xpath("././../td//a/text()")[0]
-				president_link = president_h[0].xpath("./../td//a/@href")[0]
-				print(president_link)
+				president = president_h[0].xpath("../../../td//a/text()")[0].replace(" ", "_")
+				president_link = president_h[0].xpath("../../../td//a/@href")[0]
 				pt_bdate = get_charecter_info(wiki_prefix + president_link)
 
-			print(president, cname)
+			#print(cname, president, pt_bdate)
 			prime_minister_h = infobox.xpath("//table//th[contains(text(), 'Prime Minister')]")
-			if president_h != []:
-				prime_minister = prime_minister_h[0].xpath("./../td/text()")[0]
+			if prime_minister_h != []:
+				prime_minister = prime_minister_h[0].xpath("./../td/text()")[0].areplace(" ", "_")
 				prime_minister_link = prime_minister_h[0].xpath("./../td//a/@href")[0]
 				pm_bdate = get_charecter_info(wiki_prefix + prime_minster_link)
 
 
-			population_h = infobox.xpath("//table//th[contains(text(), 'Population')]")
+			population_h = infobox.xpath("//table//th//a[contains(text(), 'Population')]")
 			if population_h != []:
-				population = population_h[0].xpath("./../td/text()")[0]
+				estimate = population_h[0].xpath("../../..//div[contains(text(), 'estimate')]")[0]
+				population = estimate.xpath("../../td//text()")[0].replace(" ", "")
+				#estimate = population_h[0].xpath("../../..//div[contains(translate(text(), 'E', 'e'), 'estimate') or contains(text(), 'census')]")[0]
 
 
-			area_h = infobox.xpath("//table//th[contains(text(), 'Area')]")
+			area_h = infobox.xpath("//table//th/a[contains(text(), 'Area')]")
 			if area_h != []:
-				area = area_h[0].xpath("./../td/text()")[0]
+				#print(area_h[0].xpath("../../../tr/th/div[contains(text(), 'Total')]"))
+				total = area_h[0].xpath("../../../tr/th[1]/div[(contains(text(), 'Total') or contains(text(), 'Land')) and position()=1]")[0]
+				print(total.xpath("../../td[1]/text()"))
+				area = total.xpath("../../td[1]/text()")[0]
 
 
-			government_h = infobox.xpath("//table//th[contains(text(), 'Government')]")
+			government_h = infobox.xpath("//table//th/a[contains(text(), 'Government')]")[0]
 			if government_h != []:
-				government = government_h[0].xpath("./../td/text()")[0]
+				government_words = government_h.xpath("../../td/a/text()")
+				for word in government_words:
+					government += (word.replace(" ", "_") + "_")
+				government = government[:-1]
+				#government = government_h[0].xpath("./../td//text()")[0]
 
 
 			capital_h = infobox.xpath("//table//th[contains(text(), 'Capital')]")
 			if capital_h != []:
-				capital = "a"#capital_h[0].xpath("./../td/text()")[0]
+				capital_lst = capital_h[0].xpath("./../td//text()")
+				if '\n' in capital_lst:
+					capital_lst.remove('\n')
+				capital = capital_lst[0].replace(" ", "_")
 
-
+			print(cname, president, pt_bdate, capital, government, population, area)
 			writer.write("<http://example.org/{0}> <http://example.org/president> <http://example.org/{1}> .\n".format(president, cname))
 			writer.write("<http://example.org/{0}> <http://example.org/prime_minister> <http://example.org/{1}> .\n".format(prime_minister, cname))
 			writer.write("<http://example.org/{0}> <http://example.org/population> <http://example.org/{1}> .\n".format(population, cname))
